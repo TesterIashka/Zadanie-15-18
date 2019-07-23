31f5a0680f436d87543d3f6b099a05f44216ba76
Alisher <test.space.job@gmail.com>
2019-07-23 11:15:12 +0300

    Исправление с учетом замечания: Возвращаемое значение при подсчете среднего арифметического тип double Убраны пункты с выбором, т.к. вывод должен быть вместе.

--- Zadanie_4/src/Zadanie_6.java
+++ Zadanie_4/src/Zadanie_6.java
@@ -12,45 +12,25 @@ public class Zadanie_6 {
         System.out.println("Введите значение z=: ");
         int cat2 = meow.nextInt();
 
-        System.out.println("Введите номер пункта, обозначающего что необходимо получить от программы:\n" +
-                "1 - Вычесть среднее арифметическое значений x,y,z\n"+
-                "2 - Получить значение среднего арифметического поделенного на 2 без остатка\n"+
-                "3 - Все вышеперечисленное\n"
-                   );
-        int cat3 = meow.nextInt();
-        switch (cat3)
-        {
-            case (1): System.out.println("Среднее арифметическое x,y,z = "+SredeeArif(cat,cat1,cat2));
-            break;
-            case (2): System.out.println("Среднее арифметическое x,y,z = "+SredeeArifBezOstatk(cat,cat1,cat2));
+
+            System.out.println("Среднее арифметическое x,y,z = "+SredeeArif(cat,cat1,cat2));
+            System.out.println("Среднее арифметическое пополам без остатка x,y,z = "+SredeeArifBezOstatk(cat,cat1,cat2));
             if (SredeeArifBezOstatk(cat,cat1,cat2)>3)
-            {
-                System.out.println("Программа выполнена корректно");
-            }
-            break;
-            case (3): System.out.println("Среднее арифметическое x,y,z = "+SredeeArif(cat,cat1,cat2));
-                      System.out.println("Среднее арифметическое x,y,z = "+SredeeArifBezOstatk(cat,cat1,cat2));
-                      if (SredeeArifBezOstatk(cat,cat1,cat2)>3)
                 {
                       System.out.println("Программа выполнена корректно");
                 }
-            break;
-
-            default:  System.out.println("Вы ввели несуществующий номер пункта");
-            break;
-        }
 
 
         }
 
-    private static int SredeeArif(int x, int y, int z)
+    private static double SredeeArif(int x, int y, int z)
     {
         return (x+y+z)/3;
     }
 
     private static int SredeeArifBezOstatk(int x1, int y1, int z1)
     {
-        return SredeeArif(x1,y1,z1)/2;
+        return (x1+y1+z1)/6;
     }
 
 }
